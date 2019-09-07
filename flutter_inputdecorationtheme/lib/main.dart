2405db3feeaff3931d3ad7ff8e48e45451d8c5b2
erniecho <erniecho@gmail.com>
2019-09-06 20:19:41 -0400

    coding them form and created agreement checkbox pg279

--- flutter_inputdecorationtheme/lib/main.dart
+++ flutter_inputdecorationtheme/lib/main.dart
@@ -33,4 +33,66 @@ class HomeWidget extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
   List<TextEditingController> _textEditingControllers = [];
   List<Widget> _widget = [];
+
+  HomeWidget({Key key}) : super(key: key) {
+    List<String> fieldNames = [
+      "First Name",
+      "Last Name",
+      "Address 1",
+      "Address 2",
+      "City",
+      "State",
+      "Zip"
+    ];
+    for (int i = 0, ii = fieldNames.length; i < ii; i++) {
+      String fieldName = fieldNames[i];
+      TextEditingController textEditingController =
+          new TextEditingController(text: "");
+      _textEditingControllers.add(textEditingController);
+      _widget.add(Padding(
+        child: _createTextFormField(fieldName, i > 1, textEditingController),
+        padding: EdgeInsets.all(10.0),
+      ));
+    }
+    _widget.add(RaisedButton(
+      onPressed: () {
+        _formKey.currentState.validate();
+      },
+      child: new Text('Save'),
+    ));
+  }
+
+  TextFormField _createTextFormField(
+      String fieldName, bool enabled, TextEditingController controller){
+    return new TextFormField(
+      enabled: enabled,
+      // ignore: missing_return
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter ${fieldName}';
+        }
+      },
+      decoration: InputDecoration(
+        icon: const Icon(Icons.person),
+        hintText: fieldName,
+        labelText: 'Enter ${fieldName}'),
+    controller: controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Input Decoration Themes"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: _widget,
+          ),),),);
+  }
+
+
 }
