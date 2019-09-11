794d287791c3f4daf5b38aad1d73f7aab99dba25
erniecho <erniecho@gmail.com>
2019-09-11 02:27:49 -0400

    complete the try cartch part of _loadEmployee.

--- flutter_http_employees/lib/main.dart
+++ flutter_http_employees/lib/main.dart
@@ -92,7 +92,7 @@ class ApiWidget extends InheritedWidget{
     return false;
   }
 
-  Future<List<Employee>>loadAndParseEmployee() async {
+  Future<List<Employee>>loadAndParseEmployees() async {
     var url = '${_BASE_URL}/employees';
     final responese = await HTTP.get(url).timeout(_TIMEOUT);
     if (responese.statusCode == 200) {
@@ -116,9 +116,167 @@ class ApiWidget extends InheritedWidget{
         }
   }
 
+  Future<dynamic> saveEmployee(Employee employee) async {
+        bool isUpdate = employee.id.isNotEmpty;
+        final uri = _BASE_URL + (isUpdate ? '/update/${employee.id}' : '/create');
+        final responese = isUpdate
+        ? await HTTP.put(url, body: json.encode(employee)).timeout(_TIMEOUT)
+            : await HTTP.post(uri, body: json.encode(employee)).timeout(_TIMEOUT);
+        if (responese.statusCode == 200) {
+          return json.decode(responese.body);
+        } else {
+          badStatusCode(responese);
+        }
+  }
 
+  Future<dynamic> deleteEmployee(String id) async {
+    final uri = '${_BASE_URL}/delete/${id}';
+    final response = await HTTP.delete(uri).timeout(_TIMEOUT);
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      badStatusCode(response);
+    }
+  }
+
+
+  badStatusCode(Response response){
+      debugPrint("Bad status code ${response.statusCode} returned from server.");
+      debugPrint("Response body ${response.body} returned from server.");
+      throw Exception(
+      'Bad status code ${response.statusCode} returned from server.');
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new ApiWidget(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new EmployeeListWidget(),),);
+  }
+}
+
+class EmployeeListWidget extends StatefulWidget {
+  @override
+  _EmployeeListWidgetState createState() => new _EmployeeListWidgetState();
 }
 
 
+class _EmployeeListWidget extends State<EmployeeListWidget> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final PleaseWaitWidget _pleaseWaitWidget =
+      PleaseWaitWidget(key: ObjectKey("pleasewaitWidget"));
 
+  bool _refresh = true;
+  List<Employee> _employees;
+  bool _pleaseWait = false;
+
+  _showSnackBar(String content, {bool error = false}) {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content:
+      Text('${error ? "An unecpected error occurred: ": ""}${content}'),
+    ));
+  }
+
+  _showPleaseWait(bool b) {
+    setState(() {
+      _pleaseWait = b;
+    });
+  }
+
+  _navigateToEmployee(BuildContext context, String employeeId) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => EmployeeDetailWidget(employeeId)),
+    ).then((result) {
+      if ((result != null) && (result is bool) && (result == true)){
+        _showSnackBar('Employee saved.');
+        _refreshEmployees();
+      }
+    });
+  }
+
+  _deleteEmployee(BuildContext context, Employee employee) async {
+    _showDeleteConfirmDialog(employee).then((result) {
+      if ((result != null) && (result is bool) && (result == true)) {
+        _showPleaseWait(true);
+        try {
+          ApiWidget.of(context).deleteEmployee(employee.id).then((employee) {
+            _showPleaseWait(false);
+            _showSnackBar('Employee deleted');
+            _refreshEmployees();
+          }).catchError((error) {
+            _showPleaseWait(false);
+            _showSnackBar(error.toString(), error: true);
+          });
+        } catch (e) {
+          _showPleaseWait(false);
+          _showSnackBar(e.toString(), error: true);
+        }
+      }
+    });
+  }
+
+
+  Future<bool> _showDeleteConfirmDialog(Employee employee) async {
+    return await showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete Employee'),
+          content: Text(
+            'Are you sure you want to delete ${employee.employeeName}?'),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () {
+                Navigator.pop(context, true);
+              },
+              child: const Text('Yes'),
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.pop(context, false);
+              },
+              child: const Text('No'),
+            )
+          ],
+        );
+      });
+  }
+
+  _refreshEmployees() {
+    setState(() {
+      _refresh = true;
+    });
+  }
+
+
+  _loadEmployees(BuildContext context) {
+    _showPleaseWait(true);
+    try {
+      ApiWidget.of(context).loadAndParseEmployees().then((employees)  {
+        employees.sort((a,b) => a.employeeName
+        .toLowerCase()
+        .compareTo(b.employeeName.toLowerCase()));
+        setState(() {
+          _employees = employees;
+        });
+          _showPleaseWait(false);
+      }).catchError((error) {
+        _showPleaseWait(false);
+        _showSnackBar(error.toString(), error: true);
+      });
+    } catch (e) {
+      _showPleaseWait(false);
+      _showSnackBar(e.toString(), error: true);
+    }
+  }
+
+
+}
 
