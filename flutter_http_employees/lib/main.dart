050a30be6a4146e031188ed81b7574eb54facc1f
erniecho <erniecho@gmail.com>
2019-09-11 21:20:02 -0400

    completed coding HTTP,API,REST & JSON application on pg322

--- flutter_http_employees/lib/main.dart
+++ flutter_http_employees/lib/main.dart
@@ -19,7 +19,7 @@ class Employee {
       this.profileImage);
 
 
-  Employee.isEmpty() {
+  Employee.empty() {
     id = "";
     employeeName = "";
     employeeSalary = "";
@@ -73,6 +73,7 @@ class PleaseWaitWidget extends StatelessWidget {
 
 
 class ApiWidget extends InheritedWidget{
+  // ignore: non_constant_identifier_names
   static final String _BASE_URL = "http://dummy.restapiexample.com/api/v1";
       static const _TIMEOUT = Duration(seconds: 10);
 
@@ -92,8 +93,9 @@ class ApiWidget extends InheritedWidget{
     return false;
   }
 
+  // ignore: missing_return
   Future<List<Employee>>loadAndParseEmployees() async {
-    var url = '${_BASE_URL}/employees';
+    var url = '$_BASE_URL/employees';
     final responese = await HTTP.get(url).timeout(_TIMEOUT);
     if (responese.statusCode == 200) {
       final parsed = json.decode(responese.body).cast<Map<String, dynamic>>();
@@ -105,8 +107,9 @@ class ApiWidget extends InheritedWidget{
     }
   }
 
+  // ignore: missing_return
   Future<Employee> loadEmployee(String id) async {
-        var url = '${_BASE_URL}/employee/${id}';
+        var url = '$_BASE_URL/employee/$id';
         final response = await HTTP.get(url).timeout(_TIMEOUT);
         if (response.statusCode == 200) {
           final parsed = json.decode(response.body);
@@ -120,7 +123,7 @@ class ApiWidget extends InheritedWidget{
         bool isUpdate = employee.id.isNotEmpty;
         final uri = _BASE_URL + (isUpdate ? '/update/${employee.id}' : '/create');
         final responese = isUpdate
-        ? await HTTP.put(url, body: json.encode(employee)).timeout(_TIMEOUT)
+        ? await HTTP.put(uri, body: json.encode(employee)).timeout(_TIMEOUT)
             : await HTTP.post(uri, body: json.encode(employee)).timeout(_TIMEOUT);
         if (responese.statusCode == 200) {
           return json.decode(responese.body);
@@ -130,7 +133,7 @@ class ApiWidget extends InheritedWidget{
   }
 
   Future<dynamic> deleteEmployee(String id) async {
-    final uri = '${_BASE_URL}/delete/${id}';
+    final uri = '$_BASE_URL/delete/$id';
     final response = await HTTP.delete(uri).timeout(_TIMEOUT);
     if (response.statusCode == 200) {
       return json.decode(response.body);
@@ -167,7 +170,7 @@ class EmployeeListWidget extends StatefulWidget {
 }
 
 
-class _EmployeeListWidget extends State<EmployeeListWidget> {
+class _EmployeeListWidgetState extends State<EmployeeListWidget> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final PleaseWaitWidget _pleaseWaitWidget =
       PleaseWaitWidget(key: ObjectKey("pleasewaitWidget"));
@@ -179,7 +182,7 @@ class _EmployeeListWidget extends State<EmployeeListWidget> {
   _showSnackBar(String content, {bool error = false}) {
     _scaffoldKey.currentState.showSnackBar(SnackBar(
       content:
-      Text('${error ? "An unecpected error occurred: ": ""}${content}'),
+      Text('${error ? "An unecpected error occurred: ": ""}$content'),
     ));
   }
 
@@ -277,6 +280,266 @@ class _EmployeeListWidget extends State<EmployeeListWidget> {
     }
   }
 
+  @override
+  Widget build(BuildContext context) {
+    if (_refresh) {
+      _refresh = false;
+      _loadEmployees(context);
+    }
 
+  ListView builder = ListView.builder(
+    itemCount: _employees != null ? _employees.length : 0,
+      itemBuilder: (context, index) {
+      Employee employee = _employees[index];
+      return ListTile(
+        title: Text('${employee.employeeName}'),
+        subtitle: Text('Age: ${employee.employeeAge}'),
+        trailing: Icon(Icons.arrow_right),
+        onTap: () => _navigateToEmployee(context, employee.id),
+        onLongPress: () => _deleteEmployee(context, employee),);
+      });
+  Widget bodyWidget = _pleaseWait
+  ? Stack(key: ObjectKey("stack"), children: [_pleaseWaitWidget, builder])
+      : Stack(key: ObjectKey("stack"), children:  [builder]);
+
+  return new Scaffold(
+      key: _scaffoldKey,
+  appBar: new AppBar(
+      title: new Text("Employees"),
+      actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.add),
+          tooltip: 'Add',
+          onPressed: () {
+            _navigateToEmployee(context, null);
+          },),
+        IconButton(
+          icon: Icon(Icons.refresh),
+          tooltip: 'Refresh',
+          onPressed: () {
+            _refreshEmployees();
+          },),
+       ],
+      ),
+      body: new Center(
+      child: bodyWidget,
+      ),);
+  }
 }
 
+// ignore: must_be_immutable
+class EmployeeDetailWidget extends StatefulWidget {
+  String _employeeId;
+
+  EmployeeDetailWidget(this._employeeId);
+
+  @override
+  _EmployeeDetailState createState() => _EmployeeDetailState(this._employeeId);
+}
+
+
+class _EmployeeDetailState extends State<EmployeeDetailWidget> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final _formKey = GlobalKey<FormState>();
+  final PleaseWaitWidget _pleaseWaitWidget =
+      PleaseWaitWidget(key: ObjectKey("pleaseWaitWidget"),);
+
+  String _employeeId;
+  bool _loaded = false;
+  bool _pleaseWait = false;
+  Employee _employee;
+  TextEditingController _nameTextController = TextEditingController();
+  TextEditingController _salaryTextController = TextEditingController();
+  TextEditingController _ageTextController = TextEditingController();
+  TextEditingController _profileImageTextController = TextEditingController();
+
+  _EmployeeDetailState(this._employeeId);
+
+  _showSnackBar(String content, {bool error = false}) {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content:
+      Text('${error ? "An unexpected error occurred:" : ""}$content'),
+    ));
+  }
+
+  _showPleaseWait(bool b) {
+    setState(() {
+      _pleaseWait = b;
+    });
+  }
+
+  TextFormField _createNameWidget() {
+    return new TextFormField(
+      // ignore: missing_return
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter the name';
+        }
+      },
+      decoration: InputDecoration(
+        icon: const Icon(Icons.person),
+        hintText: 'Name',
+        labelText: 'Enter the name'),
+        onSaved: (String value) {
+          this._employee.employeeName = value;
+        },
+        controller: _nameTextController,
+        autofocus: true,
+    );
+  }
+
+  TextFormField _createSalaryWidget() {
+    return new TextFormField(
+      // ignore: missing_return
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter the salary.';
+        }
+        int salary = int.parse(value);
+        if (salary == null) {
+          return 'Please enter the salary as a number';
+        }
+        if ((salary < 10000) || (salary > 500000)) {
+          return 'Please enter an age between 10000 and 50000.';
+        }
+      },
+      maxLength: 6,
+      maxLengthEnforced: true,
+      keyboardType: TextInputType.phone,
+      inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+      decoration: InputDecoration(
+        icon: const Icon(Icons.person),
+        hintText: 'Salary',
+        labelText: 'Enter the salary',),
+        onSaved: (String value) {
+          this._employee.employeeSalary = value;
+        },
+      controller: _salaryTextController,
+    );
+  }
+
+  TextFormField _createAgeWidget() {
+    return new TextFormField(
+      // ignore: missing_return
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter the age.';
+        }
+        int age = int.parse(value);
+        if (age == null) {
+          return 'Please enter the age as a number.';
+        }
+        if ((age < 1) || (age > 114)) {
+          return 'Please enter an age between 1 and 114';
+        }
+      },
+      maxLength: 3,
+      maxLengthEnforced: true,
+      keyboardType: TextInputType.phone,
+      inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+      decoration: InputDecoration(
+        icon: const Icon(Icons.person),
+        hintText: 'Age',
+        labelText: 'Enter the age'),
+        onSaved: (String value) {
+        this._employee.employeeAge = value;
+        },
+        controller: _ageTextController,
+    );
+  }
+
+  TextFormField _createProfileImageWidget() {
+    return new TextFormField(
+      decoration: InputDecoration(
+        icon: const Icon(Icons.person),
+        hintText: 'Profile image',
+        labelText: 'Enter the profile image'),
+      onSaved: (String value) {
+        this._employee.profileImage = value;
+      },
+      controller: _profileImageTextController,
+    );
+  }
+
+  _loadEmployee(BuildContext context) {
+    _showPleaseWait(true);
+    try {
+      ApiWidget.of(context).loadEmployee(_employeeId).then((employee) {
+        setState(() {
+          _employee = employee;
+          _nameTextController.text = employee.employeeName;
+          _salaryTextController.text = employee.employeeSalary;
+          _ageTextController.text = employee.employeeAge;
+          _profileImageTextController.text = employee.profileImage;
+        });
+        _showPleaseWait(false);
+      }).catchError((error) {
+        _showPleaseWait(false);
+        _showSnackBar(error.toString(), error: true);
+      });
+    } catch (e) {
+      _showPleaseWait(false);
+      _showSnackBar(e.toString(), error: true);
+    }
+  }
+
+  _saveEmployee(BuildContext context) {
+    _showPleaseWait(true);
+    try {
+      ApiWidget.of(context).saveEmployee(_employee).then((employee) {
+        _showPleaseWait(false);
+        Navigator.pop(context, true);
+      }).catchError((error) {
+        _showPleaseWait(false);
+        _showSnackBar(error.toString(), error: true);
+      });
+    } catch (e) {
+      _showPleaseWait(false);
+      _showSnackBar(e.toString(), error: true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_loaded) {
+      _loaded = true;
+      if (_employeeId == null) {
+        _employee = Employee.empty();
+      } else {
+        _loadEmployee(context);
+      }
+    }
+
+  List<Widget> fromWidgetList = [
+    _createNameWidget(),
+    _createSalaryWidget(),
+    _createAgeWidget(),
+    _createProfileImageWidget(),
+    RaisedButton(
+      onPressed: () {
+        if (_formKey.currentState.validate()) {
+          _formKey.currentState.save();
+          _saveEmployee(context);
+        }
+      },
+      child:  new Text('Save'),
+    )
+  ];
+  Form form = Form(key: _formKey, child: ListView(children: fromWidgetList));
+
+  Widget bodyWidget = _pleaseWait
+      ? Stack(key: ObjectKey("stack"), children: [_pleaseWaitWidget, form])
+      : Stack(key: ObjectKey("stack"), children: [form]);
+
+  return new Scaffold(
+      key: _scaffoldKey,
+      appBar: new AppBar(
+      title: new Row(children: [
+        Text("Black"),
+      Spacer(),
+      Text(_employeeId == null ? "Create Employee" : "Edit Employee")
+      ]),
+      ),
+      body: new Padding(padding: EdgeInsets.all(20.0), child: bodyWidget));
+  }
+}
