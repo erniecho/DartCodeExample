38dc638b0a74ec82f0b962197bc4d3b98af22df2
erniecho <erniecho@gmail.com>
2019-10-10 18:40:16 -0400

    incomplete error with the code for pg418

--- flutter_animated_list/lib/main.dart
+++ flutter_animated_list/lib/main.dart
@@ -81,66 +81,74 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   _buildItem(Cat cat, {int index = -1}) {
-    _MyHomePageState() : super() {
-        return ListTile(
-        key: Key("ListTile:${cat.hashCode.toString()}"),
-          leading: CircleAvatar(
-            backgroundImage: NetworkImage(cat.imageSrc),radius: 32.0),
-            title: Text(cat.name, style: TextStyle(fontSize: 25.0),),
-            subtitle: Text("This little thug is ${cat.age} year(s) old.",
-              style: TextStyle(fontSize: 15.0),),
-          onLongPress: index != null ? () => _remove(index) : null,
-        );
-      }
+    _MyHomePageState()
+    : super() {
+    return ListTile(
+    key: Key("ListTile:${cat.hashCode.toString()}"),
+    leading: CircleAvatar(
+      backgroundImage: NetworkImage(cat.imageSrc),radius: 32.0),
+    title: Text(cat.name, style: TextStyle(fontSize: 25.0),),
+    subtitle: Text("This little thug is ${cat.age} year(s) old.",
+      style: TextStyle(fontSize: 15.0),),
+    onLongPress: index != null ? () => _remove(index) : null,
+    );
+    }
 
-      _add() {
-      setState(() {
-        _cats.add(Cat("http://placekitten.com/200/${next(200, 300)}",
-          CAT_NAMES[next(0,6)], next(1, 32), 0));
-        _listKey.currentState
-    .insertItem(_cats.length - 1, duration: Duration(seconds: 2));
-      });
+    _add() {
+    setState(() {
+    _cats.add(Cat("http://placekitten.com/200/${next(200, 300)}",
+    CAT_NAMES[next(0,6)], next(1, 32), 0));
+    _listKey.currentState
+        .insertItem(_cats.length - 1, duration: Duration(seconds: 2));
+    });
     }
 
 
     _remove(int index) {
-      setState(() {
-        Cat cat = _cats[index];
-        _cats.remove(cat);
-        _listKey.currentState.removeItem(
-    index, (BuildContext context, Animation<double> animation) {
+    setState(() {
+    Cat cat = _cats[index];
+    _cats.remove(cat);
+    _listKey.currentState.removeItem(
+    index,
+    (BuildContext context, Animation<double> animation) {
     return FadeTransition(
-        opacity: animation,
-        child: _buildItem(_cats[index], index: index),
-      );
+    opacity:
+    CurvedAnimation(parent: animation, curve: Interval(0.5, 1.0)),
+    child: SizeTransition(
+    sizeFactor:
+    CurvedAnimation(parent: animation, curve: Interval(0.0, 1.0)),
+    axisAlignment: 0.0,
+    child: _buildItem(cat),
+    ),
+    );
+    },
+    duration: Duration(milliseconds: 600),
+    );
     });
-    })
-
-      });
     }
 
-  @override
-  Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(
-       title: Text(widget.title),
-     ),
-     body: AnimatedList(
-       key: _listKey,
-       initialItemCount: _cats.length,
-       itemBuilder:
-       (BuildContext context, int index, Animation<double> animation) {
-         return FadeTransition(
-           opacity: animation,
-           child: _buildItem(_cats[index], index: index),
-         );
-       },
-     ),
-     floatingActionButton: FloatingActionButton(
-       onPressed: _add,
-       tooltip: 'Increment',
-       child: Icon(Icons.add),
-     ),
-   );
+    @override
+    Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+    title: Text(widget.title),
+    ),
+    body: AnimatedList(
+    key: _listKey,
+    initialItemCount: _cats.length,
+    itemBuilder:
+    (BuildContext context, int index, Animation<double> animation) {
+    return FadeTransition(
+    opacity: animation,
+    child: _buildItem(_cats[index], index: index), //Change
+    );
+    },
+    ),
+    floatingActionButton: FloatingActionButton(
+    onPressed: _add,
+    tooltip: 'Increment',
+    child: Icon(Icons.add),
+    ),
+    );
+    }
   }
-}
