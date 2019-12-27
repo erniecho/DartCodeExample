02b5832160576a216c273a4a296d2960411e557b
erniecho <erniecho@gmail.com>
2019-12-27 00:09:57 -0500

    Learning about new debugging methods for flutter dart.

---
+++ flutter_debugging/android/app/src/main/kotlin/com/ernshu/flutter_debugging/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ernshu.flutter_debugging
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
