312c659be1e884364d2ec63cd8178a4051d02e36
erniecho <erniecho@gmail.com>
2020-04-26 21:18:00 -0400

    completed the code for coding global key share state, pg464.

--- global_key_shared_widget/lib/main.dart
+++ global_key_shared_widget/lib/main.dart
@@ -48,12 +48,85 @@ class Widget1 extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: new Icon(Icons.refresh),
-            onPressed: () => _selectPageCallback()
-          )
-        ]
+            onPressed: () => _selectPageCallback())
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text("Widget 1", textAlign: TextAlign.center, style: Theme.of(context).textTheme.display2),
+            CounterWidget(_counterWidgetGlobalKey)
+          ],
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
       )
     );
   }
-
-
 }
+
+class Widget2 extends StatelessWidget {
+  final GlobalKey _counterWidgetGlobalKey;
+  final VoidCallback _selectPageCallback;
+
+  Widget2(this._counterWidgetGlobalKey, this._selectPageCallback);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+        new IconButton(
+          icon: new Icon(Icons.refresh),
+          onPressed: () => _selectPageCallback())
+          ],
+          title: Text("Widget 2"),
+
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text("Widget 2", textAlign: TextAlign.center, style: Theme.of(context).textTheme.display2),
+          CounterWidget(_counterWidgetGlobalKey)
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+      ),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  CounterWidget(Key key) : super(key: key);
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter =0;
+
+  void _incrementCounter() {
+    setState(() {
+        _counter++;
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Text(
+        'CounterWidget',
+        style: Theme.of(context).textTheme.display2,
+      ),
+      Text('You have:', style: Theme.of(context).textTheme.display1),
+      Text(
+        '$_counter',
+        style: Theme.of(context).textTheme.display1,
+      ),
+    IconButton(
+              iconSize: 36.0,
+              icon: new Icon(Icons.add), onPressed: () => _incrementCounter()),
+    ],
+    );
+  }
+}
+
