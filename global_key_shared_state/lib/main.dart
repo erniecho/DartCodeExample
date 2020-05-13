feabfe4222f42c6f8f8a251172cc8ed7f3fa8e11
erniecho <erniecho@gmail.com>
2020-05-12 22:36:34 -0400

    complete code pg 466.

--- global_key_shared_state/lib/main.dart
+++ global_key_shared_state/lib/main.dart
@@ -39,20 +39,63 @@ class Widget1 extends StatefulWidget {
 }
 
 class _Widget1State extends State<Widget1> {
+  String _state = "some state";
+  String get state => _state;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Text("Widget1",
+          textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headline4,),
+          new Text(" State: $_state",
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.headline3)
+        ],
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+      )
+    );
   }
 }
 
 class Widget2 extends StatefulWidget {
+  Widget2({Key key}) : super(key: key);
   @override
   _Widget2State createState() => _Widget2State();
 }
 
 class _Widget2State extends State<Widget2> {
+  String _text = '';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        children:
+        [
+          Text("Widget2",
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline3),
+          Padding(
+          padding: EdgeInsets.all(20.0),
+          child: RaisedButton(
+            child: new Text("Get state from Widget1"),
+            onPressed: () {
+              setState((){
+                _text = key.currentState.state;
+              });
+            },
+          )),
+          Text("State: $_text",
+          textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline4)
+        ],
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+      ),
+    );
   }
 }
